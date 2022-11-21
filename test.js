f51dca0b890be6a99512b166411e0a1602511e0e
Ariole Dieujuste <arioledieujuste.com>
2022-11-20 20:27:19 -0500

    JavaJavaScript-Algorithms-Data-Structure

---
+++ test.js
@@ -0,0 +1,21 @@
+// var myVar;
+// myVar = 5;
+// var myNum;
+// myNum = myVar
+
+
+// Creates a new variable called myVar and assigns it an initial value of 0.
+// var myVar = 0;
+
+// Define a variable a with var and initialize it to a value of 9.
+// var a = 9
+
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+// var myFirstName = "Ariole"
+// var myLastName = "Dieujuste"
+
+
+
+
+
+
